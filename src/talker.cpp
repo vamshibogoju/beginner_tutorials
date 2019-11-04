a485bb689daf131111a56c2b46f88169262dc7af
vamshibogoju <vamshikumar0401@gmail.com>
2019-11-04 15:46:29 -0500

    fixed cpplint and cppcheck errors in the talker file and updated the results in the results folder

--- src/talker.cpp
+++ src/talker.cpp
@@ -26,7 +26,7 @@
 #include "std_msgs/String.h"
 #include "beginner_tutorials/baseOutputString.h"
 
-std::string defaultMessage = "hey this is vamshi";
+extern std::string defaultMessage = "hey this is vamshi";
 
 bool changeMessage(beginner_tutorials::baseOutputString::Request &req,
                    beginner_tutorials::baseOutputString::Response &res) {
@@ -50,18 +50,14 @@ int main(int argc, char **argv) {
 
   if (loopFreq > 0) {
     ROS_DEBUG_STREAM("loop operating at frequency of: " << loopFreq);
-  }
-
-  else if (loopFreq < 0) {
+  } else if (loopFreq < 0) {
     ROS_ERROR_STREAM("The input frequency cannot be negative");
 
     ROS_WARN_STREAM("Setting to default frequency of 10Hz");
 
     // setting loopFreq to 10 hz
     loopFreq = 10;
-  }
-
-  else if (loopFreq == 0) {
+  } else if (loopFreq == 0) {
     ROS_FATAL_STREAM("Input frequency cannot be 0Hz");
 
     ROS_WARN_STREAM("Setting to default frequency of 10Hz");
@@ -107,7 +103,8 @@ int main(int argc, char **argv) {
    */
   ros::Publisher chatter_pub = n.advertise < std_msgs::String
       > ("chatter", 1000);
-  ros::ServiceServer server = n.advertiseService("baseOutputString", changeMessage);
+  ros::ServiceServer server = n.advertiseService("baseOutputString",
+                                                 changeMessage);
 
   ros::Rate loop_rate(loopFreq);
 
